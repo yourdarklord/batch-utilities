c89451ae00a82c8d51276c118b40d076e0934996
Stefan Crowhurst <stefancrowhurst1999@yahoo.com>
2021-07-01 16:45:21 +0100

    Switching to env variables for 32-bit programs

--- ssms.bat
+++ ssms.bat
@@ -1,2 +1,2 @@
-start "" "C:\Program Files (x86)\Microsoft SQL Server Management Studio 18\Common7\IDE\Ssms.exe"
+start "" "%PROGRAMFILES(X86)%\Microsoft SQL Server Management Studio 18\Common7\IDE\Ssms.exe"
 exit
--- vs2019.bat
+++ vs2019.bat
@@ -1,2 +1,2 @@
-start "" "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\devenv.exe"
+start "" "%PROGRAMFILES(X86)%\Microsoft Visual Studio\2019\Community\Common7\IDE\devenv.exe"
 exit
